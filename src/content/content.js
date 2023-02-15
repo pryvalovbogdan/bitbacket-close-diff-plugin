82fa7b2ce5ef093630929bf5a803cf9c043b486a
bprivalov <bohdan.privalov@tech-stack.io>
2023-02-15 23:51:02 +0200

    fixed blocking description button

--- src/content/content.js
+++ src/content/content.js
@@ -1,60 +1,95 @@
 const pluginClosedClassName = 'plugin-closed';
+const arrowClosedClassName = 'plugin-arrow-closed';
 const headerSelector = 'h3[data-qa="bk-filepath"]';
-const collapseIconSelector = 'span[aria-label="collapse"]';
+const collapseIconSelector = 'button[aria-expanded="true"]';
 const diffSectionSelector = 'section[aria-label="Diffs"]';
 const commentShownSectionSelector = 'div[aria-hidden="false"]';
 const arrowExpandSelector = 'span[aria-label="Expand/collapse file"]';
-
+const collapseIconSelector2 = 'span[aria-label="collapse"]';
 const hiddenFiles = ['back-end', 'snapshots', 'test.js'];
+const includeClasses = ['ak-navigation-resize-button', 'collapse-sidebar-button', pluginClosedClassName];
 
 window.addEventListener('load', () => {
-	const collapseIcons = document.querySelectorAll(collapseIconSelector);
+		const content = document.querySelector('div[data-testid="Content"]');
 
-	collapseIcons.forEach(item => item.click());
+		const config = { attributes: false, childList: true, subtree: true };
 
-	const targetNode = document.querySelector(diffSectionSelector);
+		const callback = () => {
+			closeDiff();
 
-	if(targetNode){
-		addObserver(targetNode)
-	} else {
-		setTimeout(() => {
-			addObserver()
-		}, 2500)
-	}
+			const collapseButton = document.querySelectorAll(collapseIconSelector);
+			// console.log('collapseIcons', collapseIcons)
+			let some = [];
+
+			collapseButton.forEach(item => {
+				// console.log('closeDescription3', item.classList, includeClasses)
+				if(item.classList.contains(arrowClosedClassName)){
+					some.push(1);
+				}
+
+			});
+
+			if(some.length){
+				return;
+			}
+
+			closeDescription();
+		};
+
+		const observer = new MutationObserver(callback);
+
+		observer.observe(content, config);
+
 })
 
-function addObserver (targetNodeProps){
-	const targetNode = targetNodeProps || document.querySelector(diffSectionSelector);
+function addObserver (){
+	const targetNode = document.querySelector(diffSectionSelector);
 
 	const config = { attributes: false, childList: true, subtree: true };
 
-	closeDiff(targetNode);
+	closeDiff();
 
 	const callback = () => {
-		const targetNode = document.querySelector(diffSectionSelector);
-
-		closeDiff(targetNode)
+		// console.log('callback2')
+		closeDiff()
 	};
-
 
 	const observer = new MutationObserver(callback);
 
 	observer.observe(targetNode, config);
 }
 
-function closeDiff (targetNode) {
-	targetNode.querySelectorAll(headerSelector).forEach(item => {
-		if(hiddenFiles.some(el => item.innerText.includes(el))){
-			const parent = item.parentNode.parentNode.parentNode;
+function closeDiff () {
+	const targetNode = document.querySelector(diffSectionSelector);
 
-			if(parent.parentNode.querySelector(commentShownSectionSelector)){
-				const arrow = parent.querySelector(arrowExpandSelector);
+	if(targetNode) {
+		targetNode.querySelectorAll(headerSelector).forEach(item => {
+			if (hiddenFiles.some(el => item.innerText.includes(el))) {
+				const parent = item.parentNode.parentNode.parentNode;
 
-				if(!arrow.classList.contains(pluginClosedClassName)){
-					arrow.classList.add(pluginClosedClassName);
-					arrow.click()
+				if (parent.parentNode.querySelector(commentShownSectionSelector)) {
+					const arrow = parent.querySelector(arrowExpandSelector);
+
+					if (!arrow.classList.contains(pluginClosedClassName)) {
+						arrow.classList.add(pluginClosedClassName);
+						arrow.click()
+					}
 				}
 			}
+		});
+	}
+}
+
+function closeDescription (){
+	const collapseIcons = document.querySelectorAll(collapseIconSelector);
+	// console.log('collapseIcons', collapseIcons)
+	collapseIcons.forEach(item => {
+		// console.log('closeDescription', item.classList, includeClasses)
+		if(!includeClasses.some(el => item.getAttribute('data-testid') === el || item.classList.contains(el))){
+			item.classList.add(arrowClosedClassName);
+			item.click()
+		} else {
+			item.classList.remove(arrowClosedClassName);
 		}
 	});
 }
