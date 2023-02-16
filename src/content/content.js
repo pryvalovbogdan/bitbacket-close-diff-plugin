c51fe2c11638cc9f354da560900a8e4ea01d2997
bprivalov <bohdan.privalov@tech-stack.io>
2023-02-17 00:30:24 +0200

    added start stop plugin and settings buttons

--- src/content/content.js
+++ src/content/content.js
@@ -6,39 +6,97 @@ const diffSectionSelector = 'section[aria-label="Diffs"]';
 const commentShownSectionSelector = 'div[aria-hidden="false"]';
 const arrowExpandSelector = 'span[aria-label="Expand/collapse file"]';
 const collapseIconSelector2 = 'span[aria-label="collapse"]';
-const hiddenFiles = ['back-end', 'snapshots', 'test.js'];
-const includeClasses = ['ak-navigation-resize-button', 'collapse-sidebar-button', pluginClosedClassName];
+const hiddenFiles = ['back-end', 'snapshots', 'test.js', 'test.ts', 'package-lock.json'];
+const includeClasses = ['sidebar-expander-panel-heading', 'ak-navigation-resize-button', 'collapse-sidebar-button', pluginClosedClassName];
+
+let observerRef = null;
 
 window.addEventListener('load', () => {
-		const content = document.querySelector('div[data-testid="Content"]');
+	const pluginWrapper = document.createElement('div');
+	const pluginBtnWrapper = document.createElement('div');
+	const usePluginButton = document.createElement('button');
+	const span = document.createElement('span');
+	const hiddenSettings = document.createElement('div');
 
-		const config = { attributes: false, childList: true, subtree: true };
+	span.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" role="presentation">
+		<path d="M8.292 10.293a1.009 1.009 0 000 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 000-1.419.987.987 0 00-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 00-1.406 0z" fill="currentColor" fill-rule="evenodd">
+		</path></svg>`
 
-		const callback = () => {
-			closeDiff();
+	span.className = 'settings-button'
 
-			const collapseButton = document.querySelectorAll(collapseIconSelector);
-			// console.log('collapseIcons', collapseIcons)
-			let some = [];
+	hiddenSettings.innerHTML = `
+		<div class="checkbox-wrapper-24">
+		  <input type="checkbox" id="check-24" name="check" value="" checked />
+		  <label for="check-24">
+		    <span></span>Is Description hidden
+		  </label>
+		</div>
+		<div class="checkbox-wrapper-24">
+		  <input type="checkbox" id="check-25" name="check" value="" checked />
+		  <label for="check-25">
+		    <span></span>Is Comments hidden
+		  </label>
+		</div>
+		<div class="group">      
+      <input class="file-text-input" type="text" required>
+      <span class="highlight"></span>
+      <span class="bar"></span>
+      <label class="file-text-label">File names</label>
+    </div>
+    <button class="use-plugin-button">Apply</button>
+	`
 
-			collapseButton.forEach(item => {
-				// console.log('closeDescription3', item.classList, includeClasses)
-				if(item.classList.contains(arrowClosedClassName)){
-					some.push(1);
-				}
+	span.addEventListener('click', () => {
+		const hiddenSettings = document.querySelector('.hidden-settings');
+		console.log('hiddenSettings', hiddenSettings.classList);
+		if(hiddenSettings.classList.contains('open')){
+			hiddenSettings.style.display = 'none';
+			hiddenSettings.classList.remove('open')
+		} else {
+			hiddenSettings.style.display = 'flex';
+			hiddenSettings.classList.add('open')
+		}
+	})
 
-			});
 
-			if(some.length){
-				return;
-			}
+	let isStarted = localStorage.getItem('isStartedPlugin') === 'true';
 
+	if(isStarted){
+		startProcessingPlugin()
+	}
+
+	const textBtn = `Difference Plugin`;
+
+	const startStopPlugin = (e) => {
+		// const pluginState = !isStarted;
+		if(!isStarted){
 			closeDescription();
-		};
+			closeDiff();
+			startProcessingPlugin();
+		} else {
+			observerRef.disconnect();
+		}
+		localStorage.setItem('isStartedPlugin', `${!isStarted}`);
+		isStarted = !isStarted;
+		// const isStarted2 = localStorage.getItem('isStartedPlugin') === 'started';
 
-		const observer = new MutationObserver(callback);
+		e.target.innerText = `${isStarted ? 'Stop' : 'Start'} ${textBtn}`;
+	}
 
-		observer.observe(content, config);
+	pluginBtnWrapper.addEventListener('click', startStopPlugin);
+	console.log('isStarted', isStarted)
+	pluginBtnWrapper.className = 'plugin-btn-wrapper';
+	usePluginButton.className = 'use-plugin-button';
+	pluginWrapper.className = 'plugin-wrapper';
+	hiddenSettings.className = 'hidden-settings';
+
+	usePluginButton.innerText = `${isStarted ? 'Stop' : 'Start'} ${textBtn}`;
+
+	pluginBtnWrapper.append(usePluginButton);
+	pluginBtnWrapper.append(span);
+	pluginWrapper.append(pluginBtnWrapper);
+	pluginWrapper.append(hiddenSettings);
+	document.body.append(pluginWrapper);
 
 })
 
@@ -92,4 +150,37 @@ function closeDescription (){
 			item.classList.remove(arrowClosedClassName);
 		}
 	});
+}
+
+
+function startProcessingPlugin () {
+	const content = document.querySelector('div[data-testid="Content"]');
+
+	const config = { attributes: false, childList: true, subtree: true };
+
+	const callback = () => {
+		closeDiff();
+
+		const collapseButton = document.querySelectorAll(collapseIconSelector);
+		// console.log('collapseIcons', collapseIcons)
+		let some = [];
+
+		collapseButton.forEach(item => {
+			// console.log('closeDescription3', item.classList, includeClasses)
+			if(item.classList.contains(arrowClosedClassName)){
+				some.push(1);
+			}
+
+		});
+
+		if(some.length){
+			return;
+		}
+
+		closeDescription();
+	};
+
+	observerRef = new MutationObserver(callback);
+
+	observerRef.observe(content, config);
 }
